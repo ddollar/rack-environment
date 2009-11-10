2fb389d13440c3c6fd6f6555136bda98dba1dbf8
David Dollar <ddollar@gmail.com>
2009-11-09 22:20:49 -0500

    add rack_environment rake task

---
+++ lib/rack_environment_task.rb
@@ -0,0 +1,14 @@
+require 'rake'
+require 'rack_environment'
+
+class RackEnvironment::Task ::Rake::TaskLib
+
+  def initialize(options={})
+    rack_environment = RackEnvironment.new(options)
+
+    task :rack_environment do
+      rack_environment.update_environment!
+    end
+  end
+
+end
